5bef5266c9a979eda43549a6b153f0989969fc53
ryanf10 <ryanfernaldy10@gmail.com>
2023-11-20 19:43:57 +0700

    test: mock cache manager

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { cacheModule } from './modules.config';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 describe('AppController', () => {
   let appController: AppController;
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [cacheModule],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: () => 'any value',
+            set: () => jest.fn(),
+          },
+        },
+      ],
     }).compile();
     appController = app.get<AppController>(AppController);
   });
